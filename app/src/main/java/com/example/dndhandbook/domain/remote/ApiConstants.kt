9647f61d84fdccfae68fa14e9e4c4e5df2cb0217
Lucas Mosca <lucasbvmosca@gmail.com>
2024-06-07 23:49:42 -0300

    Feature: Create functions to fetch monster list and monster detail by index.

--- app/src/main/java/com/example/dndhandbook/domain/remote/ApiConstants.kt
+++ app/src/main/java/com/example/dndhandbook/domain/remote/ApiConstants.kt
@@ -2,6 +2,7 @@ package com.example.dndhandbook.domain.remote
 
 class ApiConstants {
     companion object {
-
+        const val FETCH_MONSTERS = "/api/monsters"
+        const val FETCH_MONSTER_DETAIL = "/api/monsters/{index}"
     }
 }
--- app/src/main/java/com/example/dndhandbook/domain/repositories/monster/MonsterRepositoryContract.kt
+++ app/src/main/java/com/example/dndhandbook/domain/repositories/monster/MonsterRepositoryContract.kt
@@ -1,4 +1,11 @@
 package com.example.dndhandbook.domain.repositories.monster
 
+import com.example.dndhandbook.domain.models.monster.MonsterDetail
+import com.example.dndhandbook.domain.models.monster.MonsterList
+
 interface MonsterRepositoryContract {
+
+    suspend fun fetchAllMonsters(): MonsterList
+
+    suspend fun fetchMonsterDetail(monsterIndex: String): MonsterDetail?
 }
