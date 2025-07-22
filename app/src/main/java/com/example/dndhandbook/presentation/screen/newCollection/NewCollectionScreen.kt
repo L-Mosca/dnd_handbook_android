1a930a6ae697511b3931cad52dfc0376b773eefe
Lucas Mosca <88050952+L-Mosca@users.noreply.github.com>
2025-07-22 18:12:43 -0300

    Feature: Create base top app bar.

--- app/src/main/java/com/example/dndhandbook/presentation/screen/newCollection/NewCollectionScreen.kt
+++ app/src/main/java/com/example/dndhandbook/presentation/screen/newCollection/NewCollectionScreen.kt
@@ -4,13 +4,19 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
-import com.example.dndhandbook.presentation.ui.theme.Gold900
+import com.example.dndhandbook.presentation.baseComponents.BaseText
+import com.example.dndhandbook.presentation.baseComponents.BaseTopBar
+import com.example.dndhandbook.presentation.ui.theme.Black800
 
 @Composable
 fun NewCollectionScreen(
@@ -20,16 +26,27 @@ fun NewCollectionScreen(
     NewCollection()
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NewCollection() {
-    Scaffold { innerPadding ->
+    Scaffold(
+        topBar = {
+            val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+            BaseTopBar(
+                title = "Teste da app bar",
+                onBackPressed = {},
+                scrollBehavior = scrollBehavior
+            )
+        }
+    ) { innerPadding ->
         LazyColumn(
             modifier = Modifier
                 .padding(innerPadding)
-                .background(color = Gold900)
+                .background(color = Black800)
                 .fillMaxSize(),
-        ) { }
+        ) {
 
+        }
     }
 }
 
