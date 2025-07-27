0dee967f26390ab7390e37aea5f1b579fda9cbdf
Lucas Mosca <lucasbvmosca@gmail.com>
2025-07-27 18:43:30 -0300

    Feature: Add create and empty collection design on HomeCollection.kt.

--- app/src/main/java/com/example/dndhandbook/presentation/screen/home/components/HomeCollection.kt
+++ app/src/main/java/com/example/dndhandbook/presentation/screen/home/components/HomeCollection.kt
@@ -5,7 +5,9 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -18,6 +20,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -30,14 +33,15 @@ import com.example.dndhandbook.presentation.baseComponents.BaseText
 import com.example.dndhandbook.presentation.ui.theme.Black700
 import com.example.dndhandbook.presentation.ui.theme.Black800
 import com.example.dndhandbook.presentation.ui.theme.Crimson800
+import com.example.dndhandbook.presentation.ui.theme.Gold700
 import com.example.dndhandbook.presentation.ui.theme.Gray100
 
 @Composable
 fun HomeCollection(
     collectionList: List<MonsterCollection> = emptyList(),
     onCollectionClicked: ((MonsterCollection) -> Unit)? = null,
+    addCollectionClicked: (() -> Unit)? = null,
 ) {
-
     Surface(
         shape = RoundedCornerShape(topStart = 10.dp, topEnd = 10.dp),
         color = Black700,
@@ -46,19 +50,32 @@ fun HomeCollection(
         Column(
             modifier = Modifier.padding(20.dp)
         ) {
-            Title()
-            CollectionList(collectionList, onCollectionClicked)
+            Title(addCollectionClicked)
+            if (collectionList.isNotEmpty()) CollectionList(collectionList, onCollectionClicked)
+            if (collectionList.isEmpty()) CollectionEmptyList()
         }
     }
 }
 
 @Composable
-private fun Title() {
-    BaseText(
-        text = stringResource(R.string.collections),
-        fontSize = 20.sp,
-        fontWeight = FontWeight.W700,
-    )
+private fun Title(addCollectionClicked: (() -> Unit)? = null) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        BaseText(
+            text = stringResource(R.string.collections),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.W700,
+            modifier = Modifier.weight(1f)
+        )
+        Icon(
+            painter = painterResource(R.drawable.ic_add),
+            contentDescription = stringResource(R.string.create_new_collection),
+            tint = Crimson800,
+            modifier = Modifier.size(30.dp).clickable { addCollectionClicked?.invoke() }
+        )
+    }
 }
 
 @Composable
@@ -67,9 +84,7 @@ private fun CollectionList(
     onCollectionClicked: ((MonsterCollection) -> Unit)? = null,
 ) {
     LazyColumn(
-        modifier = Modifier
-            .background(Black700)
-            .padding(top = 20.dp),
+        modifier = Modifier.background(Black700),
         verticalArrangement = Arrangement.spacedBy(6.dp),
     ) {
         itemsIndexed(collectionList) { index, collection ->
@@ -79,8 +94,11 @@ private fun CollectionList(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(horizontal = 10.dp)
+                    .padding(
+                        top = if (index == 0) 50.dp else 4.dp,
+                        bottom = if (index == collectionList.lastIndex) 30.dp else 0.dp,
+                    )
                     .clickable { onCollectionClicked?.invoke(collection) }
-
             ) {
                 Row(
                     modifier = Modifier.padding(horizontal = 20.dp, vertical = 4.dp),
@@ -109,14 +127,43 @@ private fun CollectionList(
     }
 }
 
+@Composable
+private fun CollectionEmptyList() {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Spacer(Modifier.height(50.dp))
+        BaseText(
+            text = stringResource(R.string.not_collections_found),
+            fontSize = 18.sp,
+            color = Gold700
+        )
+        Spacer(Modifier.height(10.dp))
+        Icon(
+            painter = painterResource(R.drawable.ic_box),
+            contentDescription = stringResource(R.string.not_collections_found),
+            tint = Crimson800,
+            modifier = Modifier.size(100.dp),
+        )
+        Spacer(Modifier.height(30.dp))
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun HomeCollectionPreview() {
     val collections = listOf(
-        MonsterCollection(name ="nome da coleção", monsterList =  emptyList()),
-        MonsterCollection(name ="nome da coleção", monsterList =  emptyList()),
-        MonsterCollection(name ="nome da coleção", monsterList =  emptyList()),
-        MonsterCollection(name ="nome da coleção", monsterList =  emptyList()),
+        MonsterCollection(name = "nome da coleção", monsterList = emptyList()),
+        MonsterCollection(name = "nome da coleção", monsterList = emptyList()),
+        MonsterCollection(name = "nome da coleção", monsterList = emptyList()),
+        MonsterCollection(name = "nome da coleção", monsterList = emptyList()),
     )
     HomeCollection(collections)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeCollectionEmptyPreview() {
+    HomeCollection()
 }
