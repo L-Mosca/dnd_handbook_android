50df10d0c2fd66e5d0e37165a2922ebeedcdca9f
Lucas Mosca <lucasbvmosca@gmail.com>
2025-07-27 18:43:30 -0300

    Feature: Add create and empty collection design on HomeCollection.kt.

--- app/src/main/java/com/example/dndhandbook/presentation/screen/home/HomeScreen.kt
+++ app/src/main/java/com/example/dndhandbook/presentation/screen/home/HomeScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -22,17 +21,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.example.dndhandbook.R
 import com.example.dndhandbook.domain.models.collection.MonsterCollection
 import com.example.dndhandbook.navigation.BestiaryRoute
 import com.example.dndhandbook.navigation.NewCollectionNavGraph
-import com.example.dndhandbook.presentation.baseComponents.BaseText
 import com.example.dndhandbook.presentation.screen.home.components.HomeCollection
 import com.example.dndhandbook.presentation.ui.theme.Black800
 import kotlinx.coroutines.flow.distinctUntilChanged
@@ -88,19 +83,13 @@ private fun Home(
         ) {
             //CreateCharacterButton(navController = navController)
             BestiaryButton(onBestiaryClicked)
-            Spacer(Modifier.height(20.dp))
-            BaseText(
-                text = stringResource(R.string.create_new_collection),
-                fontSize = 20.sp,
-                modifier = Modifier
-                    .clickable { onNewCollectionClicked?.invoke() }
-            )
 
             Spacer(Modifier.weight(1f))
 
             HomeCollection(
                 collectionList = collectionList,
                 onCollectionClicked = { onCollectionClicked?.invoke(it) },
+                addCollectionClicked = onNewCollectionClicked,
             )
         }
     }
