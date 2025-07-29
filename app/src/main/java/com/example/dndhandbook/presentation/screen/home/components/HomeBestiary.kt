fbda0099187141b50423cacdf9f89a071d3c3938
Lucas Mosca <lucasbvmosca@gmail.com>
2025-07-29 18:07:52 -0300

    Feature: Add loading, error and empty base place holders.

--- app/src/main/java/com/example/dndhandbook/presentation/screen/home/components/HomeBestiary.kt
+++ app/src/main/java/com/example/dndhandbook/presentation/screen/home/components/HomeBestiary.kt
@@ -1,23 +1,58 @@
 package com.example.dndhandbook.presentation.screen.home.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.dndhandbook.R
+import com.example.dndhandbook.presentation.baseComponents.BaseText
 import com.example.dndhandbook.presentation.baseComponents.gif.BaseGif
+import com.example.dndhandbook.presentation.ui.theme.Black700
 
 @Composable
 fun HomeBestiary(onBestiaryClicked: (() -> Unit)? = null) {
-    BaseGif(
-        gifRes = R.drawable.gif_error,
-        modifier = Modifier.size(200.dp),
-        show = true
-    )
+
+    Surface(
+        shape = RoundedCornerShape(topEnd = 10.dp, topStart = 10.dp),
+        color = Black700,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Column(modifier = Modifier.padding(20.dp)) {
+            BaseText(
+                text = stringResource(R.string.bestiary),
+                fontSize = 20.sp,
+                fontWeight = FontWeight.W700,
+            )
+            Spacer(Modifier.height(50.dp))
+            BaseGif(
+                gifRes = R.drawable.gif_beholder,
+                repeatCount = 0,
+                show = true,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.CenterHorizontally)
+                    .size(180.dp)
+                    .clickable { onBestiaryClicked?.invoke() },
+            )
+            Spacer(Modifier.height(30.dp))
+        }
+    }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun HomeBestiaryPreview() {
     HomeBestiary()
