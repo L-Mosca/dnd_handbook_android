aca347d8ee82048480841847935e60f55898dcba
Lucas Mosca <lucasbvmosca@gmail.com>
2025-07-30 22:01:29 -0300

    Refactor: remove navController from MonsterDetailScreen.

--- app/src/main/java/com/example/dndhandbook/navigation/BestiaryNavGraph.kt
+++ app/src/main/java/com/example/dndhandbook/navigation/BestiaryNavGraph.kt
@@ -39,7 +39,7 @@ fun NavGraphBuilder.bestiaryNavGraph(navController: NavHostController) {
         }
 
         animatedComposable<MonsterDetailRoute> {
-            MonsterDetailScreen(navController)
+            MonsterDetailScreen()
         }
     }
 }
