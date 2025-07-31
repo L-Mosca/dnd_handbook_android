00d6eb6d72752e66e1fabf28dbcdb5bae064e6bc
Lucas Mosca <lucasbvmosca@gmail.com>
2025-07-30 23:48:33 -0300

    Improvement: delete empty collections on back action.

--- app/src/main/java/com/example/dndhandbook/presentation/screen/newCollection/NewCollectionUIState.kt
+++ app/src/main/java/com/example/dndhandbook/presentation/screen/newCollection/NewCollectionUIState.kt
@@ -1,10 +1,8 @@
 package com.example.dndhandbook.presentation.screen.newCollection
 
-import com.example.dndhandbook.domain.models.base.DefaultObject
 import com.example.dndhandbook.domain.models.collection.MonsterCollection
 
 data class NewCollectionUIState(
-    val monster: DefaultObject? = null,
     val collection: MonsterCollection = MonsterCollection(),
     val saveSuccess: Boolean = false,
 )
