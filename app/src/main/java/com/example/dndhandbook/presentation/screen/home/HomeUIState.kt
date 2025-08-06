a8ab0fca0f06385de6ee91f1df14cb0a55c0990d
Lucas Mosca <lucasbvmosca@gmail.com>
2025-08-06 16:54:46 -0300

    Refactor: Add default launch on BaseViewModel to handle coroutine scopes.

--- app/src/main/java/com/example/dndhandbook/presentation/screen/home/HomeUIState.kt
+++ app/src/main/java/com/example/dndhandbook/presentation/screen/home/HomeUIState.kt
@@ -4,4 +4,8 @@ import com.example.dndhandbook.domain.models.collection.MonsterCollection
 
 data class HomeUIState(
     val collectionList: List<MonsterCollection> = emptyList(),
-)
+) {
+    fun setCollectionList(list: List<MonsterCollection>) = copy(
+        collectionList = list
+    )
+}
