0d9a431039b4a78bcf749d8644794460f3ec21e7
Lucas Mosca <lucasbvmosca@gmail.com>
2025-08-06 14:28:12 -0300

    Refactor: Remove unused code from MonsterDetailScreen.

--- app/src/main/java/com/example/dndhandbook/presentation/screen/monsterDetail/MonsterDetailViewModel.kt
+++ app/src/main/java/com/example/dndhandbook/presentation/screen/monsterDetail/MonsterDetailViewModel.kt
@@ -5,10 +5,7 @@ import androidx.lifecycle.viewModelScope
 import androidx.navigation.toRoute
 import com.example.dndhandbook.base.BaseViewModel
 import com.example.dndhandbook.common.Resource
-import com.example.dndhandbook.domain.models.base.DefaultObject
-import com.example.dndhandbook.domain.models.monster.MonsterDetail
 import com.example.dndhandbook.domain.useCase.bestiary.getMonster.GetMonsterUseCase
-import com.example.dndhandbook.domain.useCase.collection.updateCollectionUseCase.UpdateCollectionUseCase
 import com.example.dndhandbook.navigation.MonsterDetailRoute
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -17,14 +14,12 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MonsterDetailViewModel @Inject constructor(
     private val getMonsterDetailUseCase: GetMonsterUseCase,
-    private val updateCollectionUseCase: UpdateCollectionUseCase,
-    savedStateHandle: SavedStateHandle
+    savedStateHandle: SavedStateHandle,
 ) : BaseViewModel() {
 
     private val _uiState = MutableStateFlow(MonsterDetailState())
@@ -32,19 +27,14 @@ class MonsterDetailViewModel @Inject constructor(
 
     private var isFromCollection = false
     private var monsterIndex: String
-    //private var collectionId: Long? = null
 
     init {
         savedStateHandle.toRoute<MonsterDetailRoute>().let { args ->
             isFromCollection = args.isFromCollection
             monsterIndex = args.monsterIndex
-            //collectionId = args.collectionId
 
             _uiState.update {
-                it.copy(
-                    isFromCollection = args.isFromCollection,
-                    //collectionId = args.collectionId,
-                )
+                it.copy(isFromCollection = args.isFromCollection)
             }
 
             getMonsterDetail()
@@ -54,35 +44,10 @@ class MonsterDetailViewModel @Inject constructor(
     fun getMonsterDetail() {
         getMonsterDetailUseCase(monsterIndex).onEach { result ->
             when (result) {
-                is Resource.Success -> showMonsterDetail(result.data ?: MonsterDetail())
-                is Resource.Loading -> showLoading()
-                is Resource.Error -> showError()
+                is Resource.Success -> _uiState.update { it.setMonsterDetail(result.data) }
+                is Resource.Loading -> _uiState.update { it.showLoading() }
+                is Resource.Error -> _uiState.update { it.showError() }
             }
         }.launchIn(viewModelScope)
-    }
-
-    fun saveMonsterDetail(monster: DefaultObject) {
-        viewModelScope.launch {
-            //updateCollectionUseCase.invoke(collectionId, monster)
-            _uiState.update { it.copy(navigateBack = true) }
-        }
-    }
-
-    private fun showMonsterDetail(monsterDetail: MonsterDetail) {
-        _uiState.update {
-            it.copy(
-                monsterDetail = monsterDetail,
-                isLoading = false,
-                showError = false,
-            )
-        }
-    }
-
-    private fun showLoading() {
-        _uiState.update { it.copy(isLoading = true, showError = false, monsterDetail = null) }
-    }
-
-    private fun showError() {
-        _uiState.update { it.copy(isLoading = false, showError = true, monsterDetail = null) }
     }
 }
