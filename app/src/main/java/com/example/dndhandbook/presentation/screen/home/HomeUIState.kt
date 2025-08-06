3cd01244b14c8eaaac948018379b344ba539caf5
Lucas Mosca <lucasbvmosca@gmail.com>
2025-08-06 14:38:20 -0300

    Refactor: Remove unused code from HomeScreen.

--- app/src/main/java/com/example/dndhandbook/presentation/screen/home/HomeUIState.kt
+++ app/src/main/java/com/example/dndhandbook/presentation/screen/home/HomeUIState.kt
@@ -4,5 +4,4 @@ import com.example.dndhandbook.domain.models.collection.MonsterCollection
 
 data class HomeUIState(
     val collectionList: List<MonsterCollection> = emptyList(),
-    val collectionSelected: MonsterCollection? = null,
 )
