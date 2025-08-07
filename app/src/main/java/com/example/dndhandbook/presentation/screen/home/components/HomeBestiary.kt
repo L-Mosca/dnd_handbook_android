5617606aa56309527da0220a0ee22638a8352a1c
Lucas Mosca <lucasbvmosca@gmail.com>
2025-08-07 00:16:42 -0300

    Refactor: Update home design.

--- app/src/main/java/com/example/dndhandbook/presentation/screen/home/components/HomeBestiary.kt
+++ app/src/main/java/com/example/dndhandbook/presentation/screen/home/components/HomeBestiary.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -15,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -22,22 +22,36 @@ import com.example.dndhandbook.R
 import com.example.dndhandbook.presentation.baseComponents.BaseText
 import com.example.dndhandbook.presentation.baseComponents.gif.BaseLottieGif
 import com.example.dndhandbook.presentation.ui.theme.Black700
+import com.example.dndhandbook.presentation.ui.theme.Gray300
 
 @Composable
 fun HomeBestiary(onBestiaryClicked: (() -> Unit)? = null) {
 
     Surface(
-        shape = RoundedCornerShape(topEnd = 10.dp, topStart = 10.dp),
+        shape = RoundedCornerShape(10.dp),
         color = Black700,
-        modifier = Modifier.fillMaxWidth().offset(x = 0.dp, y = 10.dp)
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 20.dp, start = 20.dp, end = 20.dp),
     ) {
-        Column(modifier = Modifier.padding(20.dp).fillMaxWidth()) {
+        Column(
+            modifier = Modifier
+                .padding(20.dp)
+                .fillMaxWidth()
+        ) {
             BaseText(
                 text = stringResource(R.string.bestiary),
-                fontSize = 20.sp,
+                fontSize = 22.sp,
                 fontWeight = FontWeight.W700,
             )
-            Spacer(Modifier.height(50.dp))
+            Spacer(Modifier.height(12.dp))
+            BaseText(
+                text = stringResource(R.string.bestiary_detail),
+                fontSize = 18.sp,
+                textAlign = TextAlign.Start,
+                color = Gray300,
+            )
+            Spacer(Modifier.height(30.dp))
             BaseLottieGif(
                 rawRes = R.raw.monster_eye_anim,
                 iterations = 1,
@@ -46,7 +60,7 @@ fun HomeBestiary(onBestiaryClicked: (() -> Unit)? = null) {
                     .size(180.dp)
                     .clickable { onBestiaryClicked?.invoke() },
             )
-            Spacer(Modifier.height(30.dp))
+            Spacer(Modifier.height(10.dp))
         }
     }
 }
