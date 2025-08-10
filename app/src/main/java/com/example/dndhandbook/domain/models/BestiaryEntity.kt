f177adca05e57bdfc5d5f4f20bb1c8508cef563f
Lucas Mosca <lucasbvmosca@gmail.com>
2025-08-10 16:09:52 -0300

    feature: add data on bestiary dao and use in monster screens

--- app/src/main/java/com/example/dndhandbook/domain/models/BestiaryEntity.kt
+++ app/src/main/java/com/example/dndhandbook/domain/models/BestiaryEntity.kt
@@ -3,6 +3,7 @@ package com.example.dndhandbook.domain.models
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.dndhandbook.domain.models.base.DefaultList
 import com.example.dndhandbook.domain.models.base.DefaultObject
 
 @Entity(tableName = "bestiary_table")
@@ -10,4 +11,7 @@ data class BestiaryEntity(
     @PrimaryKey(autoGenerate = true) @ColumnInfo("id") val id: Long = 0,
     @ColumnInfo("count") val count: Int = 0,
     @ColumnInfo("results") val results: List<DefaultObject> = emptyList(),
-)
+) {
+
+    fun toDefaultList(): DefaultList = DefaultList(count = count, results = results)
+}
