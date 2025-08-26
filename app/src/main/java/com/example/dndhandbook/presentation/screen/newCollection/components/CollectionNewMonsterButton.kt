12702cd0a8a6620710b48de9e7c6ad14201d4cd2
Lucas-Mosca <110690274+Lucas-Mosca@users.noreply.github.com>
2025-08-26 16:18:09 -0300

    feature: add base dialog class

--- app/src/main/java/com/example/dndhandbook/presentation/screen/newCollection/components/CollectionNewMonsterButton.kt
+++ app/src/main/java/com/example/dndhandbook/presentation/screen/newCollection/components/CollectionNewMonsterButton.kt
@@ -8,7 +8,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.dndhandbook.R
-import com.example.dndhandbook.presentation.baseComponents.BaseButton
+import com.example.dndhandbook.presentation.baseComponents.button.BaseButton
 
 @Composable
 fun CollectionNewMonsterButton(onClick: (() -> Unit) = {}) {
